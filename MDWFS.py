5e9830f41651a18b83c3acbce722697936e9b348
Knowlesspot <57564070+Knowlesspot@users.noreply.github.com>
2022-03-18 11:08:04 +0000

    version Sync

--- MDWFS.py
+++ MDWFS.py
@@ -1,6 +1,5 @@
 from tkinter import N
 from pyo import *
-
 def exciter(noiseType='white', mul=1, add=0):
     if noiseType == 'white':
         n = Noise(mul, add)
@@ -15,8 +14,10 @@ def getPitch(voice):
     print(pitch)
     return pitch
 SAMPLERATE = 44100
+BUFFERSIZE = 128
+
 #Set server settings and boot
-s = Server(sr=SAMPLERATE)
+s = Server(sr=SAMPLERATE, buffersize=256, duplex=0)
 s.setMidiInputDevice(99)
 s.boot()
 s.start()
@@ -28,11 +29,11 @@ pit = note["pitch"]
 freqs = MToF(pit)
 amp = MidiAdsr(note["velocity"], attack=0.001, decay=0.1, sustain=0.7, release=0, mul=0.1,)
 
-exc = exciter(noiseType='brown', mul=amp)
+exc = exciter(noiseType='white', mul=amp)
 
-d = Delay(exc, 1/freqs, 0.99)
-
-#d.setInput(filt + exc)
+d = Delay(exc, 1/freqs, 0)
+filt = exc + (0.99*d)
+d.setInput(filt)
 d.out()
 s.gui(locals())
 
